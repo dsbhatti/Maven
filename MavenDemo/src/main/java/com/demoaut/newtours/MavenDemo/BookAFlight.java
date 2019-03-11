3ce63361369ad79d3c767aa43480c5a4921dc875
deepinder <deepinder@whitecircleschool.com>
2019-03-11 14:28:09 -0400

    Comment Added

--- MavenDemo/src/main/java/com/demoaut/newtours/MavenDemo/BookAFlight.java
+++ MavenDemo/src/main/java/com/demoaut/newtours/MavenDemo/BookAFlight.java
@@ -23,6 +23,8 @@ public class BookAFlight {
 		PageFactory.initElements(driver, this);
 	}
 	
+	//Comment
+	
 	public void flightBooking(String fName, String lName, String creditCardNumber) {
 		firstName.sendKeys(fName);
 		lastName.sendKeys(lName);
